fa4d939d9132f4416f984c0c0f71062ab26b0d8b
İrem Gülten <iremgulten@gmail.com>
2021-08-11 09:46:44 +0300

    Changed validation rule

--- BookStore.Business/FluentValidation/RegisterModelValidator.cs
+++ BookStore.Business/FluentValidation/RegisterModelValidator.cs
@@ -8,7 +8,7 @@ namespace BookStore.Business.FluentValidation
         public RegisterModelValidator()
         {
             RuleFor(x => x.Username).NotEmpty().WithMessage("Username field cannot be empty");
-            RuleFor(x => x.Email).EmailAddress()
+            RuleFor(x => x.Email).EmailAddress().WithMessage("‘Email’ is not a valid email address.")
                                  .NotEmpty().WithMessage("Email field cannot be empty");
             RuleFor(x => x.Password).NotEmpty().WithMessage("Password field cannot be empty")
                                     .MinimumLength(8).WithMessage("The length of ‘Password’ must be 8 characters or fewer."); 
--- BookStore.Business/FluentValidation/UserLoginModelValidator.cs
+++ BookStore.Business/FluentValidation/UserLoginModelValidator.cs
@@ -13,7 +13,7 @@ namespace BookStore.Business.FluentValidation
         public UserLoginModelValidator()
         {
             RuleFor(x => x.Username).NotEmpty().WithMessage("Username field cannot be empty");
-            RuleFor(x => x.Email).EmailAddress()
+            RuleFor(x => x.Email).EmailAddress().WithMessage("‘Email’ is not a valid email address.")
                                  .NotEmpty().WithMessage("Email field cannot be empty");
             RuleFor(x => x.Password).NotEmpty().WithMessage("Password field cannot be empty")
                                     .MinimumLength(8).WithMessage("The length of ‘Password’ must be 8 characters or fewer.");
